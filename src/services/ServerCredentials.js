3480507bcc3f659fefcea95a1f973b9d73d1ba61
Gokulakannan <gokulatsecon@gmail.com>
2026-03-16 10:32:26 +0530

    changes

--- src/services/ServerCredentials.js
+++ src/services/ServerCredentials.js
@@ -1,5 +1,5 @@
 var GeoSeverPassword = "geoserver";
 var GeoSeverUser = "admin";
-export const GEOSERVER_URL = '/geoserver/';
+export const GEOSERVER_URL = '/geoserver';
 export const AUTH_HEADER = 'Basic ' + btoa(GeoSeverUser + ':' + GeoSeverPassword);
 export const WORKSPACE = 'gisweb';
